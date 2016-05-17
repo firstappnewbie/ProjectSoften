4e07ea119df6d3ec8bb6c147b46171d9069f6b45
3arthzneiz <3arthzneiz@3arthzneizs-MacBook-Pro.local>
2016-05-17 17:17:31 +0700

    update ui and function

---
+++ ProjectSE/ProjectSE/tabbarVC.swift
@@ -0,0 +1,35 @@
+//
+//  tabbarVC.swift
+//  ProjectSE
+//
+//  Created by 3arthzneiz on 5/17/2559 BE.
+//  Copyright © 2559 3arthzneiz. All rights reserved.
+//
+
+import UIKit
+
+class tabbarVC: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
